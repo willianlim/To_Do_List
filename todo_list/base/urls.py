6462354f717d4b503c6956afceec868e799634c4
Willian-code <willianrosendodelima@outlook.com>
2022-09-07 19:11:16 -0300

    detail page

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,8 +1,8 @@
-from nturl2path import url2pathname
 from django.urls import path
-from .views import TaskList
+from .views import TaskList, TaskDetail
 
 
 urlpatterns = [
 	path('', TaskList.as_view(), name='tasks'),
+	path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
 ]
